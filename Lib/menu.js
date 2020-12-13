afdb5825e13d5da599f1099daa29d5107b640963
Bayu dea <75917807+Bayu60t@users.noreply.github.com>
2020-12-14 05:35:17 +0700

    Update menu.js

--- Lib/menu.js
+++ Lib/menu.js
@@ -2,10 +2,11 @@ exports.menu = (id, BotName, corohelp, tampilTanggal, tampilWaktu, instagram, te
 	return `⚜*${BotName}*⚜
   
 HALO *${id.split("@s.whatsapp.net")[0]}* 
-APA KABAR? SEMOGA BAIK-BAIK SAJA!
+Berikut adalah *MENU BANTUAN* yang terdapat di dalam *BLACK DRAGON*
+
 Sebelum menggunakan bot ini ada baiknya kalian melihat menu *!info* dan *!donasi* 😁
 
-
+Ayo bantu *BLACK DRAGON*  Agar bot dapat meringankan pembiayaan server, dengan cara berdonasi,ketik *!donasi* untuk berdonasi
 
 
 📅*${tampilTanggal}* ⚡️
@@ -22,23 +23,23 @@ Sebelum menggunakan bot ini ada baiknya kalian melihat menu *!info* dan *!donasi
 _${BotName}_akan mengirimkanmu pantun secara acak
 
 📌 _*!animepict*_
-_${BotName}_🍄akan mengirimkanmu gambar anime secara random
+_${BotName}_akan mengirimkanmu gambar anime secara random
 
-🐼 _*!sticker*_
-_${BotName}_🍄akan membuatkan sticker dari gambar yang kamu kirimkan
+📌 _*!sticker*_
+_${BotName}_akan membuatkan sticker dari gambar yang kamu kirimkan
 
 Pengggunaan : Kirimkan gambarmu dengan caption !sticker
 
-🐼 _*!nulis <teks>*_
-_${BotName}_🍄akan menuliskan teks yang kamu kirimkan
+📌 _*!nulis <teks>*_
+_${BotName}_akan menuliskan teks yang kamu kirimkan
 
 Contoh: !nulis pasti nana bangka dadang ko bang jamping jamping 
 
-🐼 _*!quotes*_
-_${BotName}_🍄akan mencarikanmu quotes secara random
+📌 _*!quotes*_
+_${BotName}_akan mencarikanmu quotes secara random
 
-🐼 _*!pict <cewek/cowok>*_
-_${BotName}_🍄akan mengirimkanmu gambar cewek/cowok secara random
+📌 _*!pict <cewek/cowok>*_
+_${BotName}_akan mengirimkanmu gambar cewek/cowok secara random
 Contoh: !pict cowok
 
 
@@ -61,38 +62,38 @@ contoh : !alay ampun bang jago
 
 ☪*ISLAM*☪
 
-🐼 _*!sholat <daerah>*_
-_${BotName}_🍄akan mengirimkan jadwal sholat sesuai dengan daerah yang kamu kirimkan
+📌 _*!sholat <daerah>*_
+_${BotName}_akan mengirimkan jadwal sholat sesuai dengan daerah yang kamu kirimkan
 
 Penggunaan 	: !sholat + daerah kamu
 Contoh		: !sholat Surabaya
 
-🐼 _*!quran*_
-_${BotName}_🍄akan mengirimkanmu ayat Al-Quran secara random
+📌 _*!quran*_
+_${BotName}_akan mengirimkanmu ayat Al-Quran secara random
 
 📥*DOWNLOADER*📥
 
-🐼 _*!yt <link>*_
-_${BotName}_🍄akan mendownloadkan video youtube sesuai dengan link yang kamu kirimkan.
+📌 _*!yt <link>*_
+_${BotName}_akan mendownloadkan video youtube sesuai dengan link yang kamu kirimkan.
 Contoh: !yt https://youtu.be/linkamu
 
-🐼 _*!ytmp3 <link>*_
-_${BotName}_🍄akan mengubah video youtube menjadi audio sesuai dengan link yang kamu kirimkan
+📌 _*!ytmp3 <link>*_
+_${BotName}_akan mengubah video youtube menjadi audio sesuai dengan link yang kamu kirimkan
 
 Contoh: !ytmp3 https://youtu.be/linkamu
 
-🐼 _*!ig <link>*_
-_${BotName}_🍄akan mengirimkanmu foto/video dari link ig yang kamu kirimkan
+📌 _*!ig <link>*_
+_${BotName}_akan mengirimkanmu foto/video dari link ig yang kamu kirimkan
 
 Contoh: !ig https://instagram.com/linkamu
 
-🐼 _*!fb <link>*_ //ERROR//
-_${BotName}_🍄akan mengirimkanmu foto/video dari link ig yang kamu kirimkan
+📌 _*!fb <link>*_ //ERROR//
+_${BotName}_akan mengirimkanmu foto/video dari link ig yang kamu kirimkan
 
 Contoh: !fb https://facebook.com/linkamu
 
-🐼 _*!twt <link>*_
-_${BotName}_🍄akan mengirimkanmu foto/video dari link ig yang kamu kirimkan
+📌 _*!twt <link>*_
+_${BotName}_akan mengirimkanmu foto/video dari link ig yang kamu kirimkan
 
 Contoh: !twt https://twitter.com/linkamu
 
@@ -102,38 +103,36 @@ _${BotName}_ akan mengirimkanmu video dari link tiktok yang kamu kirimkan
 
 🖥*EDUKASI*🖥
 
-🐼 _*!wikia <yang kamu cari>*_
-_${BotName}_🍄akan mencarikan yang kamu mau di wikipedia
+📌 _*!wikia <yang kamu cari>*_
+_${BotName}_akan mencarikan yang kamu mau di wikipedia
 
 Contoh: !wikia Adolf Hitler
 
-🐼 _*!covid*_
-_${BotName}_🍄akan menampilkan data tentang COVID-19 di Indonesia
+📌 _*!covid*_
+_${BotName}_akan menampilkan data tentang COVID-19 di Indonesia
 
 🐰*PRIMBON*🐰
 
-🐼 _*!nama <namakamu>*_
-_${BotName}_🍄akan mencarikan apa arti dari nama kamu
+📌 _*!nama <namakamu>*_
+_${BotName}_akan mencarikan apa arti dari nama kamu
 
 Contoh : !nama Stalin
 
-🐼 _*!pasangan <namamu & pasangan>*_
-_${BotName}_🍄akan mencarikan kecocokan antara kamu dan pasanganmu
+📌 _*!pasangan <namamu & pasangan>*_
+_${BotName}_akan mencarikan kecocokan antara kamu dan pasanganmu
 
 Contoh : !pasangan covad & covid
 
+➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖
 
-⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇
-FOLLOW INSTAGRAM SAYA
+FOLLOW INSTAGRAM mimin 
 ${instagram}
 
-• TELEGRAM : ${telegram}
-• Youtube : R E U Z
-• WA : 081271699856
+
+• Youtube : tetesan air mata
+• WA : +62 822-2549-1446
 • Pasang Iklan? Chat Whatsapp.
 
-📺 *IKLAN* : *YOUTUBE REUZ*
-🏚SELALU GUNAKAN MASKER SAAT KELUAR!🏚
-🏚SEMOGA SEHAT SELALU!🏚
+🙏  *gunakan bot secara bijak dan benar*  🙏
 `
 }
